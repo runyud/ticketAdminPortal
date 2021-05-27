625990d89072712de36c1e28890af10e37578987
runyud <runyud@umich.edu>
2021-05-27 19:05:17 -0400

    update

--- src/main/java/com/adminportal/service/UserService.java
+++ src/main/java/com/adminportal/service/UserService.java
@@ -3,7 +3,6 @@ package com.adminportal.service;
 import java.util.Set;
 
 import com.adminportal.domain.User;
-import com.adminportal.domain.security.PasswordResetToken;
 import com.adminportal.domain.security.UserRole;
 
 public interface UserService {
